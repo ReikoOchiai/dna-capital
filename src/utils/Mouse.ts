f9b37146c505be7172ce31fe9955b397f06738ab
Reiko Ochiai <reikoochiai@Reikos-MacBook-Pro.local>
2024-01-25 10:54:20 +1100

    EventEmitter import fixed and particle shader added

--- src/utils/Mouse.ts
+++ src/utils/Mouse.ts
@@ -1,45 +1,45 @@
-import EventEmitter from "events";
+import { EventEmitter } from 'events'
 
 // src
-import Experience from "../Experience.ts";
+import Experience from '../Experience.ts'
 
 // utils
-import Sizes from "./Sizes.ts";
+import Sizes from './Sizes.ts'
 
 type PlanePosition = {
-  x: number;
-  y: number;
-};
+	x: number
+	y: number
+}
 
 export default class Mouse extends EventEmitter {
-  experience: Experience | null;
-  sizes: Sizes;
-  cursorPosition: PlanePosition;
-  scrollPosition: PlanePosition;
+	experience: Experience | null
+	sizes: Sizes
+	cursorPosition: PlanePosition
+	scrollPosition: PlanePosition
 
-  constructor() {
-    super();
+	constructor() {
+		super()
 
-    this.experience = new Experience();
-    this.sizes = this.experience.sizes;
+		this.experience = new Experience()
+		this.sizes = this.experience.sizes
 
-    this.cursorPosition = {
-      x: window.scrollX,
-      y: window.scrollY,
-    };
-    this.scrollPosition = {
-      x: 0,
-      y: 0,
-    };
+		this.cursorPosition = {
+			x: window.scrollX,
+			y: window.scrollY,
+		}
+		this.scrollPosition = {
+			x: 0,
+			y: 0,
+		}
 
-    window.addEventListener("mousemove", (event) => {
-      this.cursorPosition.x = (event.clientX / this.sizes.width) * 2 - 1;
-      this.cursorPosition.y = (-event.clientY / this.sizes.height) * 2 + 1;
-    });
+		window.addEventListener('mousemove', (event) => {
+			this.cursorPosition.x = (event.clientX / this.sizes.width) * 2 - 1
+			this.cursorPosition.y = (-event.clientY / this.sizes.height) * 2 + 1
+		})
 
-    window.addEventListener("scroll", () => {
-      this.scrollPosition.x = window.scrollX;
-      this.scrollPosition.y = window.scrollY;
-    });
-  }
+		window.addEventListener('scroll', () => {
+			this.scrollPosition.x = window.scrollX
+			this.scrollPosition.y = window.scrollY
+		})
+	}
 }
