040a5a712e16ef9ef920b9e53e5c03e0017e2d9d
Reiko Ochiai <lissage.23@gmail.com>
2024-02-13 10:03:25 +1100

    update assets for this project

--- src/constants/assets.ts
+++ src/constants/assets.ts
@@ -1,18 +1,19 @@
 export type Asset = {
-  name: string;
-  type: "gltfModel" | "texture" | "cubeTexture" | "audio" | "video" | "font";
-  path: string;
-};
+	name: string
+	type:
+		| 'gltfModel'
+		| 'texture'
+		| 'cubeTexture'
+		| 'audio'
+		| 'video'
+		| 'font'
+	path: string
+}
 
 export const assets: Asset[] = [
-  {
-    name: "boxModel",
-    type: "gltfModel",
-    path: "models/boxModel.glb",
-  },
-  {
-    name: "boxTexture",
-    type: "texture",
-    path: "textures/boxTexture.png",
-  },
-];
+	{
+		name: 'dnaModel',
+		type: 'gltfModel',
+		path: 'models/dna.glb',
+	},
+]
