3faf957ff541dec54f1adc3f8be4bd9a7dc083ff
Reiko Ochiai <lissage.23@gmail.com>
2024-02-14 10:01:01 +1100

    update the partical size

--- src/shaders/vertex_particles_shader.glsl
+++ src/shaders/vertex_particles_shader.glsl
@@ -16,6 +16,6 @@ void main() {
     vUv = uv;
     vRandomColors = randomColors;
     vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
-    gl_PointSize = (20.0 * randoms) * (1.0/ -mvPosition.z);
+    gl_PointSize = (50.0 * randoms) * (1.0/ -mvPosition.z);
     gl_Position = projectionMatrix * mvPosition;
 }
