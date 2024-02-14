27e07a40d2db6866fe8fb672b1e9cf0c06ecde86
Reiko Ochiai <lissage.23@gmail.com>
2024-02-14 21:29:14 +1100

    add GUI to check appropriate parameters for bloom effect

--- src/Renderer.ts
+++ src/Renderer.ts
@@ -8,7 +8,12 @@ import { RenderPass } from 'three/addons/postprocessing/RenderPass.js'
 import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js'
 // utils
 import Sizes from './utils/Sizes.ts'
-
+import Debug from './utils/Debug.ts'
+type BloomEffectSettings = {
+	threshold: number
+	strength: number
+	radius: number
+}
 export default class Renderer {
 	experience: Experience
 	sizes: Sizes
@@ -19,17 +24,46 @@ export default class Renderer {
 	renderScene: RenderPass
 	composer: EffectComposer
 	bloomPass: UnrealBloomPass
+	settings: BloomEffectSettings
+	debug: Debug
+
 	constructor() {
 		this.experience = new Experience()
 		this.sizes = this.experience.sizes
 		this.scene = this.experience.scene
 		this.canvas = this.experience.canvas
 		this.camera = this.experience.camera
+		this.debug = this.experience.debug
 
+		this.settings = {
+			threshold: 0.001,
+			strength: 0.8,
+			radius: 0.10,
+		}
 		this.setRenderer()
 		this.initpost()
 	}
 	initpost() {
+		// GUI settings
+		// window.location.hash = 'debug' // Activate GUI
+		// if (this.debug.active) {
+		// 	this.debug.gui
+		// 		.add(this.settings, 'threshold', 0, 1)
+		// 		.name('Bloom threshold')
+		// 		.step(0.0001)
+		// 		.onChange((value) => (this.bloomPass.threshold = value))
+		// 	this.debug.gui
+		// 		.add(this.settings, 'strength', 0, 10)
+		// 		.name('Bloom strength')
+		// 		.step(0.001)
+		// 		.onChange((value) => (this.bloomPass.strength = value))
+		// 	this.debug.gui
+		// 		.add(this.settings, 'radius', 0, 10)
+		// 		.name('Bloom radius')
+		// 		.step(0.001)
+		// 		.onChange((value) => (this.bloomPass.radius = value))
+		// }
+
 		this.renderScene = new RenderPass(this.scene, this.camera.perspectiveCamera)
 
 		this.bloomPass = new UnrealBloomPass(
@@ -38,6 +72,10 @@ export default class Renderer {
 			0.9,
 			0.85
 		)
+		this.bloomPass.threshold = this.settings.threshold
+		this.bloomPass.strength = this.settings.strength
+		this.bloomPass.radius = this.settings.radius
+
 		this.composer = new EffectComposer(this.renderer)
 		this.composer.addPass(this.renderScene)
 		this.composer.addPass(this.bloomPass)
